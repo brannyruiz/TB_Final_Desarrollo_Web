d849968590d9aef0927bc7a1280ee39572923cf4
Branny Ruiz <61120777+brannyruiz@users.noreply.github.com>
2024-05-02 13:07:16 -0500

    Agregando Component carritoCompra

--- BananitoStore/src/app/app-routing.module.ts
+++ BananitoStore/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { CarritoCompraComponent } from './pages/carrito-compra/carrito-compra.component';
 
 const router: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'carritoCompra', component: CarritoCompraComponent },
 ];
 
 @NgModule({
--- BananitoStore/src/app/app.routes.ts
+++ BananitoStore/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { CarritoCompraComponent } from './pages/carrito-compra/carrito-compra.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'carritoCompra', component: CarritoCompraComponent },
 ];

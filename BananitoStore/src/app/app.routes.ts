72ad1030f2ff73e76440c4ecf88ed1dde6b1da2c
Danny H. <dhmontano1984@gmail.com>
2024-05-03 17:39:20 -0500

    Agregar pagina nosotros, ofertas, marcas y contactos

--- BananitoStore/src/app/app.routes.ts
+++ BananitoStore/src/app/app.routes.ts
@@ -2,9 +2,18 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { CarritoCompraComponent } from './pages/carrito-compra/carrito-compra.component';
+import { MarcasComponent } from './pages/marcas/marcas.component';
+import { OfertasComponent } from './pages/ofertas/ofertas.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ContactenosComponent } from './pages/contactenos/contactenos.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'carritoCompra', component: CarritoCompraComponent },
+  { path: 'ofertas', component: OfertasComponent},
+  { path: 'marcas', component: MarcasComponent},
+  { path: 'nosotros', component: NosotrosComponent},
+  { path: 'contactenos', component: ContactenosComponent}
+
 ];

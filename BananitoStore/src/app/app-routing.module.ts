3807df370156bd865c3ea29c61987e8548fc67b2
Branny Ruiz <61120777+brannyruiz@users.noreply.github.com>
2024-05-04 13:20:44 -0500

    Agregando Component detalleProducto

--- BananitoStore/src/app/app-routing.module.ts
+++ BananitoStore/src/app/app-routing.module.ts
@@ -3,11 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { CarritoCompraComponent } from './pages/carrito-compra/carrito-compra.component';
+import { OfertasComponent } from './pages/ofertas/ofertas.component';
+import { MarcasComponent } from './pages/marcas/marcas.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ContactenosComponent } from './pages/contactenos/contactenos.component';
+import { DetalleCompraComponent } from './pages/detalle-compra/detalle-compra.component';
 
 const router: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'carritoCompra', component: CarritoCompraComponent },
+  { path: 'ofertas', component: OfertasComponent },
+  { path: 'marcas', component: MarcasComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'contactenos', component: ContactenosComponent },
+  { path: 'detalleCompra', component: DetalleCompraComponent },
 ];
 
 @NgModule({
--- BananitoStore/src/app/app.routes.ts
+++ BananitoStore/src/app/app.routes.ts
@@ -6,14 +6,15 @@ import { MarcasComponent } from './pages/marcas/marcas.component';
 import { OfertasComponent } from './pages/ofertas/ofertas.component';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 import { ContactenosComponent } from './pages/contactenos/contactenos.component';
+import { DetalleCompraComponent } from './pages/detalle-compra/detalle-compra.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'carritoCompra', component: CarritoCompraComponent },
-  { path: 'ofertas', component: OfertasComponent},
-  { path: 'marcas', component: MarcasComponent},
-  { path: 'nosotros', component: NosotrosComponent},
-  { path: 'contactenos', component: ContactenosComponent}
-
+  { path: 'ofertas', component: OfertasComponent },
+  { path: 'marcas', component: MarcasComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'contactenos', component: ContactenosComponent },
+  { path: 'detalleCompra', component: DetalleCompraComponent },
 ];
